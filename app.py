e36691ed0144ffa282701857db60f59d77514ade
Muk3sh <mukeshmyke95@gmail.com>
2021-08-02 17:36:49 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
-#Basic Flask Application
 from flask import Flask
+from flask import json
+import logging
+
 app = Flask(__name__)
 
-#ENd point - Helloworld homepage
+@app.route('/status')
+def healthcheck():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    app.logger.info('Status request successfull')
+    return response
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    app.logger.info('Metrics request successfull')
+    return response
+
 @app.route("/")
 def hello():
+    app.logger.info('Main request successfull')
+
     return "Hello World!"
 
 if __name__ == "__main__":
+    ## stream logs to a file
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
+    
     app.run(host='0.0.0.0')
